5cacd97dd8fb9585bef547fe56e267d69a2c29a4
Jaakko Lehtonen <>
2021-12-05 17:35:14 +0200

    Complete exercise 13.9

--- util/middlewares.js
+++ util/middlewares.js
@@ -1,5 +1,10 @@
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
+
+  if (error.name === "SequelizeValidationError") {
+    return res.status(400).send({ error: error.errors.map((e) => e.message) });
+  }
+
   return res.status(400).send({ error });
 };
 
